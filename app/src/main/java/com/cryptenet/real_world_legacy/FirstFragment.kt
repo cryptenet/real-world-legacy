bdeaaefea3c9ffa018678a26b3cec16ba79baf6e
Alamgir Hossain <alamgir.h@cryptenet.com>
2021-11-14 22:22:13 +0600

    [CU-1up2kjk] - project instantiated
    - README updated
    - project renamed
    - package renamed

--- app/src/main/java/com/cryptenet/real_world_legacy/FirstFragment.kt
+++ app/src/main/java/com/cryptenet/real_world_legacy/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.cryptenet.legacy_base
+package com.cryptenet.real_world_legacy
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,14 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.cryptenet.legacy_base.databinding.FragmentSecondBinding
+import com.cryptenet.real_world_legacy.databinding.FragmentFirstBinding
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class FirstFragment : Fragment() {
 
-    private var _binding: FragmentSecondBinding? = null
+    private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,17 +23,17 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 
